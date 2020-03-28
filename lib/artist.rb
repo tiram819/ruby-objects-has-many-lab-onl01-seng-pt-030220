f0d39f8619248aafa0958ac06138cba4026abf15
Tabassum Hassan <tiram819@gmail.com>
2020-03-28 15:52:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,8 +17,8 @@ class Artist
 
   def add_song_by_name(name)
     song = Song.new(name)
-    @songs << song
-    song.artist = self
+    #@songs << song
+    #song.artist = self
     @@song_count += 1
   end
 
